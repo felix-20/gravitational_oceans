b6d9d01995695d84fd0577955034616412c4dae3
felix-20 <39854388+felix-20@users.noreply.github.com>
2022-10-26 14:59:37 +0200

    started on `data_generator`
    Co-Authored-By: Storyxx <57802448+Storyxx@users.noreply.github.com>

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
+import os
 from kaggle.api.kaggle_api_extended import KaggleApi
+
+if not os.path.isdir('./data'):
+    os.makedirs('./data')
 
 api = KaggleApi()
 api.authenticate()
@@ -6,8 +10,10 @@ api.authenticate()
 # download single file
 # Signature: dataset_download_file(dataset, file_name, path=None, force=False, quiet=True)
 all_files = api.competitions_data_list_files('g2net-detecting-continuous-gravitational-waves')
-with open('all_files.txt', 'w') as file:
+with open('./data/all_files.txt', 'w') as file:
     for f in all_files:
         file.write(str(f) + '\n')
 
-api.dataset_download_file('g2net-detecting-continuous-gravitational-waves', 'sample_submission.csv')
+
+os.chdir('./data')
+os.system('kaggle competitions download g2net-detecting-continuous-gravitational-waves -f sample_submission.csv')
