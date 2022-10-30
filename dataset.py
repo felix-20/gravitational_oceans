e68f4c47213b85d96723e93418cfeafb80fb74a5
Stroyxx <rk81ogel@gmail.com>
2022-10-30 11:55:10 +0100

    make training work

--- dataset.py
+++ dataset.py
@@ -39,18 +39,16 @@ class GODataset(Dataset):
         result = []
         for time_amplitudes in stfts:
             sampled_time_amplitudes = time_amplitudes[np.random.choice(len(time_amplitudes), size=time_samples, replace=False)]
-            transformed_sampled_time_amplitudes = np.column_stack((sampled_time_amplitudes.real, sampled_time_amplitudes.imag))
+            transformed_sampled_time_amplitudes = np.column_stack((sampled_time_amplitudes.real, sampled_time_amplitudes.imag, np.zeros(time_samples)))
             result += [transformed_sampled_time_amplitudes]
 
-        result = np.array(result)
-        assert result.shape == (360, time_samples, 2)
+        result = np.transpose(np.array(result), axes=(2, 0, 1))
+        assert result.shape == (3, 360, time_samples)
+        print_red(result[0][0][0])
         return result
 
     def __len__(self) -> int:
         return len(self.frame)
 
     def __getitem__(self, idx) -> dict:
-        stft_data = self.frame[idx][0]
-        transformed_stft_data = np.transpose(stft_data, axes=(2, 0, 1))
-        print_red('in dataset.py->GODataset->__get_item__ after transform', transformed_stft_data.shape)
-        return (transformed_stft_data, self.frame[idx][1])
+        return self.frame[idx]
--- efficientnet.py
+++ efficientnet.py
@@ -34,6 +34,10 @@ for epoch in range(2):  # loop over the dataset multiple times
         # get the inputs; data is a list of [inputs, labels]
         inputs, labels = data
 
+        # upload to gpu
+        inputs = inputs.to(device).float()
+        labels = labels.to(device)
+
         # zero the parameter gradients
         optimizer.zero_grad()
 
@@ -44,9 +48,7 @@ for epoch in range(2):  # loop over the dataset multiple times
         optimizer.step()
 
         # print statistics
-        running_loss += loss.item()
-        if i % 2 == 0:    # print every 2000 mini-batches
-            print(f'[{epoch + 1}, {i + 1:5d}] loss: {running_loss / 2000:.3f}')
-            running_loss = 0.0
+        item_loss = loss.item()
+        print(f'[{epoch + 1}, {i + 1:5d}] loss: {item_loss}')
 
 print('Finished Training')
