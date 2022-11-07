c20540e235ab54e23a68304366c518a86edbbcdd
felix-20 <39854388+felix-20@users.noreply.github.com>
2022-11-07 18:34:20 +0100

    started on visualizing data
    Co-Authored-By: Storyxx <57802448+Storyxx@users.noreply.github.com>

--- src/data_management/data_timeline.py
+++ src/data_management/data_timeline.py
@@ -1,5 +1,5 @@
 import os
-
+import numpy as np
 import h5py
 
 from src.helper.priority_queue import GOPriorityQueue
@@ -11,8 +11,10 @@ if not os.path.isdir(PATH_TO_TMP_FOLDER):
 
 time_queue = GOPriorityQueue()
 frequency_queue = GOPriorityQueue()
+m = np.array([])
 
-def process_file(path_to_file):
+def process_file(path_to_file: str, label: bool) -> None:
+    global m
     with h5py.File(path_to_file, 'r') as hd5_file:
         base_key = list(hd5_file.keys())[0]
         timestamps = hd5_file[f'{base_key}/H1/timestamps_GPS']
@@ -24,18 +26,24 @@ def process_file(path_to_file):
         print('------------------')
 
         # TODO: are there overlapping timestamps or frequencies??
+        x, is_duplicate_time = time_queue.insert(time_tuple)
+        y, is_duplicate_frequency = frequency_queue.insert(frequency_tuple)
+        print(m.shape)
+        size = m.shape[-1] + 1
 
-        if time_tuple in time_queue.queue:
-            print_yellow(f'time tuple {time_tuple} already exists')
-        else:
-            time_queue.insert(time_tuple)
-            print_green(f'put time tuple {time_tuple}')
+        if not is_duplicate_time:
+            m = np.c_[m[:x], np.zeros(size),m[x:]]
+        if not is_duplicate_frequency:
+            tmp_m = np.transpose(m)
+            size = tmp_m.shape[-1]
+            tmp_m = np.c_[tmp_m[:x], np.zeros(size),tmp_m[x:]]
+            m = np.transpose(tmp_m)
+        
+        m[x:y] = int(label) + 1
+        print(x,y)
+        print(m)
+        input()
 
-        if frequency_tuple in frequency_queue.queue:
-            print_yellow(f'frequency tuple {frequency_tuple} already exists')
-        else:
-            frequency_queue.insert(frequency_tuple)
-            print_green(f'put frequency tuple {frequency_tuple}')
 
 if not os.path.isfile(f'{PATH_TO_TEST_FOLDER}/train_labels.csv'):
     os.system(f'kaggle competitions download g2net-detecting-continuous-gravitational-waves -p {PATH_TO_TEST_FOLDER} -f train_labels.csv')
@@ -67,4 +75,4 @@ with open(f'{PATH_TO_TEST_FOLDER}/train_labels.csv', 'r') as file:
             os.system(f'unzip -qn -d {path_to_folder} {path_to_zip_file}')
             os.system(f'rm {path_to_zip_file}')
 
-        process_file(final_file)
+        process_file(final_file, ground_truth == '1')
--- src/helper/priority_queue.py
+++ src/helper/priority_queue.py
@@ -3,34 +3,33 @@ class GOPriorityQueue:
     def __init__(self) -> None:
         self.queue = []
 
-    def insert(self, element: tuple[float, float]) -> int:
+    def insert(self, element: tuple[float, float]) -> tuple[int, bool]:
         if not self.queue:
             self.queue += [element]
-            return 0
-        has_been_inserted = False
+            return (0, False)
+
         for i in range(len(self.queue)):
             item = self.queue[i]
-
             # check for interval intersection
             if element[0] < item[1] and item[0] < element[1]:
-                raise ValueError(f'intervall intersection: {element}, {item}')
+                return (i, True)
 
             if element[1] < item[0]:
                 # the new element needs to be inserted before the current item
-                self.queue = self.queue[:i] + [element] + self.queue[i+1:]
-                return i
-
-        if not has_been_inserted:
-            # the element is larger than all already inserted elements
-            self.queue += [element]
-            return len(self.queue) - 1
+                self.queue = self.queue[:i] + [element] + self.queue[i:]
+                return (i, False)
+        # the element is larger than all already inserted elements
+        self.queue += [element]
+        return (len(self.queue) - 1, False)
 
     def __str__(self) -> str:
         return str(self.queue)
 
 if __name__ == '__main__':
     a = GOPriorityQueue()
-    a.insert((1.01, 2.02))
-    a.insert((4.02, 5.02))
-    a.insert((0.3, 0.8))
-    a.insert((0.0, 0.02))
+    print(a.insert((1.01, 2.02)))
+    print(a.insert((4.02, 5.02)))
+    print(a.insert((0.3, 0.8)))
+    print(a.insert((0.0, 0.02)))
+    print(a.insert((2.5, 3.0)))
+    print(a)
