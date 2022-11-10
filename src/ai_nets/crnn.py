d1d120b0a709ab9ef23d4883f8665e6a096fad72
felix-20 <39854388+felix-20@users.noreply.github.com>
2022-11-10 13:26:50 +0100

    tried crnn
    Co-Authored-By: Storyxx <57802448+Storyxx@users.noreply.github.com>

---
+++ src/ai_nets/crnn.py
@@ -0,0 +1,152 @@
+# https://github.com/dredwardhyde/crnn-ctc-loss-pytorch
+import sys
+from itertools import groupby
+
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.utils.data as data_utils
+import torchvision.transforms.functional as TF
+from torchvision import datasets, transforms
+from tqdm import tqdm
+from colorama import Fore
+
+from src.data_management.dataset import GODataset
+
+epochs = 5
+num_classes = 2
+blank_label = 2
+image_height = 360
+gru_hidden_size = 128
+gru_num_layers = 2
+cnn_output_height = 4
+cnn_output_width = 32
+digits_per_sequence = 100
+number_of_sequences = 10000
+dataset_sequences = []
+dataset_labels = []
+
+device = torch.device('gpu' if torch.cuda.is_available() else 'cpu')
+print(f'Using {device} for training')
+
+seq_dataset = GODataset()# data_utils.TensorDataset(dataset_data, dataset_labels)
+train_set, val_set = torch.utils.data.random_split(seq_dataset,
+                                                   [int(len(seq_dataset) * 0.8), int(len(seq_dataset) * 0.2)])
+
+train_loader = torch.utils.data.DataLoader(train_set, batch_size=64, shuffle=True)
+val_loader = torch.utils.data.DataLoader(val_set, batch_size=1, shuffle=True)
+
+# ================================================= MODEL ==============================================================
+class CRNN(nn.Module):
+
+    def __init__(self):
+        super(CRNN, self).__init__()
+        self.conv1 = nn.Conv2d(1, 32, kernel_size=(3, 3))
+        self.norm1 = nn.InstanceNorm2d(32)
+        self.conv2 = nn.Conv2d(32, 32, kernel_size=(3, 3), stride=2)
+        self.norm2 = nn.InstanceNorm2d(32)
+        self.conv3 = nn.Conv2d(32, 64, kernel_size=(3, 3))
+        self.norm3 = nn.InstanceNorm2d(64)
+        self.conv4 = nn.Conv2d(64, 64, kernel_size=(3, 3), stride=2)
+        self.norm4 = nn.InstanceNorm2d(64)
+        self.gru_input_size = cnn_output_height * 64
+        self.gru = nn.GRU(self.gru_input_size, gru_hidden_size, gru_num_layers, batch_first=True, bidirectional=True)
+        self.fc = nn.Linear(gru_hidden_size * 2, num_classes)
+
+    def forward(self, x):
+        batch_size = x.shape[0]
+        out = self.conv1(x)
+        out = self.norm1(out)
+        out = F.leaky_relu(out)
+        out = self.conv2(out)
+        out = self.norm2(out)
+        out = F.leaky_relu(out)
+        out = self.conv3(out)
+        out = self.norm3(out)
+        out = F.leaky_relu(out)
+        out = self.conv4(out)
+        out = self.norm4(out)
+        out = F.leaky_relu(out)
+        out = out.permute(0, 3, 2, 1)
+        out = out.reshape(batch_size, -1, self.gru_input_size)
+        out, _ = self.gru(out)
+        out = torch.stack([F.log_softmax(self.fc(out[i]), dim=-1) for i in range(out.shape[0])])
+        return out
+
+
+model = CRNN().to(device)
+criterion = nn.CTCLoss(blank=blank_label, reduction='mean', zero_infinity=True)
+optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
+
+# ================================================ TRAINING MODEL ======================================================
+for _ in range(epochs):
+    # ============================================ TRAINING ============================================================
+    train_correct = 0
+    train_total = 0
+    for x_train, y_train in tqdm(train_loader,
+                                 position=0, leave=True,
+                                 file=sys.stdout, bar_format="{l_bar}%s{bar}%s{r_bar}" % (Fore.GREEN, Fore.RESET)):
+        batch_size = x_train.shape[0]  # x_train.shape == torch.Size([64, 28, 140])
+        x_train = x_train.view(x_train.shape[0], 1, x_train.shape[1], x_train.shape[2])
+        optimizer.zero_grad()
+        y_pred = model(x_train.to(device))
+        y_pred = y_pred.permute(1, 0, 2)  # y_pred.shape == torch.Size([64, 32, 11])
+        input_lengths = torch.IntTensor(batch_size).fill_(cnn_output_width)
+        target_lengths = torch.IntTensor([len(t) for t in y_train])
+        loss = criterion(y_pred, y_train, input_lengths, target_lengths)
+        loss.backward()
+        optimizer.step()
+        _, max_index = torch.max(y_pred, dim=2)  # max_index.shape == torch.Size([32, 64])
+        for i in range(batch_size):
+            raw_prediction = list(max_index[:, i].detach().cpu().numpy())  # len(raw_prediction) == 32
+            prediction = torch.IntTensor([c for c, _ in groupby(raw_prediction) if c != blank_label])
+            if len(prediction) == len(y_train[i]) and torch.all(prediction.eq(y_train[i])):
+                train_correct += 1
+            train_total += 1
+    print('TRAINING. Correct: ', train_correct, '/', train_total, '=', train_correct / train_total)
+
+    # ============================================ VALIDATION ==========================================================
+    val_correct = 0
+    val_total = 0
+    for x_val, y_val in tqdm(val_loader,
+                             position=0, leave=True,
+                             file=sys.stdout, bar_format="{l_bar}%s{bar}%s{r_bar}" % (Fore.BLUE, Fore.RESET)):
+        batch_size = x_val.shape[0]
+        x_val = x_val.view(x_val.shape[0], 1, x_val.shape[1], x_val.shape[2])
+        y_pred = model(x_val.to(device))
+        y_pred = y_pred.permute(1, 0, 2)
+        input_lengths = torch.IntTensor(batch_size).fill_(cnn_output_width)
+        target_lengths = torch.IntTensor([len(t) for t in y_val])
+        criterion(y_pred, y_val, input_lengths, target_lengths)
+        _, max_index = torch.max(y_pred, dim=2)
+        for i in range(batch_size):
+            raw_prediction = list(max_index[:, i].detach().cpu().numpy())
+            prediction = torch.IntTensor([c for c, _ in groupby(raw_prediction) if c != blank_label])
+            if len(prediction) == len(y_val[i]) and torch.all(prediction.eq(y_val[i])):
+                val_correct += 1
+            val_total += 1
+    print('TESTING. Correct: ', val_correct, '/', val_total, '=', val_correct / val_total)
+
+# ============================================ TESTING =================================================================
+number_of_test_imgs = 10
+test_loader = torch.utils.data.DataLoader(val_set, batch_size=number_of_test_imgs, shuffle=True)
+test_preds = []
+(x_test, y_test) = next(iter(test_loader))
+y_pred = model(x_test.view(x_test.shape[0], 1, x_test.shape[1], x_test.shape[2]).to(device))
+y_pred = y_pred.permute(1, 0, 2)
+_, max_index = torch.max(y_pred, dim=2)
+for i in range(x_test.shape[0]):
+    raw_prediction = list(max_index[:, i].detach().cpu().numpy())
+    prediction = torch.IntTensor([c for c, _ in groupby(raw_prediction) if c != blank_label])
+    test_preds.append(prediction)
+
+for j in range(len(x_test)):
+    mpl.rcParams["font.size"] = 8
+    plt.imshow(x_test[j], cmap='gray')
+    mpl.rcParams["font.size"] = 18
+    plt.gcf().text(x=0.1, y=0.1, s="Actual: " + str(y_test[j].numpy()))
+    plt.gcf().text(x=0.1, y=0.2, s="Predicted: " + str(test_preds[j].numpy()))
+    plt.show()
