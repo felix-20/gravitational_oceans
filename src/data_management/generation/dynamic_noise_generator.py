1394cc78a47d54f8b79a5b341ad8c66683632569
Stroyxx <rk81ogel@gmail.com>
2023-03-05 10:16:53 +0100

    dynamic noise ready for charlie

--- src/data_management/generation/dynamic_noise_generator.py
+++ src/data_management/generation/dynamic_noise_generator.py
@@ -15,32 +15,26 @@ from src.helper.utils import PATH_TO_DYNAMIC_NOISE_FOLDER, PATH_TO_CACHE_FOLDER,
 from src.data_management.generation.timestamps_generator import GOTimestepGenerator
 
 class GODynamicNoiseGenerator:
-    def __init__(self, timesteps: list, statistics: GOStatistics = GOStatistics(), buckets: int = 256) -> None:
+    def __init__(self, timesteps: list, statistics: GOStatistics = GOStatistics(), bucket_count: int = 512) -> None:
         self.constants = statistics
         self.timestep_count = len(timesteps)
         self.timesteps = timesteps
         self.statistics = statistics
         self.dynamic_noise_stats = statistics.noise.dynamic
-        self.buckets = buckets
+        self.bucket_count = bucket_count
 
-    def linear_time_buckets(self, bucket_count):
-        bucket_size = (self.timesteps[-1] - self.timesteps[0]) // bucket_count
-        idx = np.searchsorted(self.timesteps, [self.timesteps[0] + bucket_size * i for i in range(bucket_count)])
+    def linear_time_buckets(self):
+        bucket_size = (self.timesteps[-1] - self.timesteps[0]) // self.bucket_count
+        idx = np.searchsorted(self.timesteps, [self.timesteps[0] + bucket_size * i for i in range(self.bucket_count)])
         return list(idx) + [self.timestep_count]
     
-    def bucketize_real_noise_asd(self, sfts, bucket_count=256):
-        bucket_size = (self.timesteps[-1] - self.timesteps[0]) // bucket_count
-        idx = np.searchsorted(self.timesteps, [self.timesteps[0] + bucket_size * i for i in range(bucket_count)])
-        global_noise_amp = np.mean(np.abs(sfts))
-        return np.array([np.mean(np.abs(i)) if i.shape[1] > 0 else global_noise_amp for i in np.array_split(sfts, idx[1:], axis=1)]), bucket_size
-
-    def generate_noise(self, bucket_count: int = 512, token: str='none'):
-        bucket_delimiter_indices = self.linear_time_buckets(bucket_count)
+    def generate_noise(self):
+        bucket_delimiter_indices = self.linear_time_buckets(self.bucket_count)
         single_bucket_sizes = np.ediff1d(bucket_delimiter_indices)
 
         buckets = []
 
-        for i in range(bucket_count):
+        for i in range(self.bucket_count):
             if single_bucket_sizes[i] == 0:
                 continue
 
@@ -51,22 +45,11 @@ class GODynamicNoiseGenerator:
 
             buckets += [np.random.normal(bucket_mean_offset + bucket_mean, bucket_std, (360, single_bucket_sizes[i]))]
 
-        # generate statistics
-        result = np.concatenate(buckets, axis=1)
-        #means = [np.mean(bucket) for bucket in np.array_split(result, bucket_count, axis=1)]
-        #means = np.mean(result, axis=0)
-        means, _ = self.bucketize_real_noise_asd(result, bucket_count)
-        x = range(bucket_count)#range(self.timestep_count)
-        plt.plot(x, means)
-        plt.savefig(join(PATH_TO_CACHE_FOLDER, 'direct_statistics_'+token+'.png'))
-        plt.clf()
-
         return np.concatenate(buckets, axis=1)
     
     def __call__(self, idx: int):
-
         token = token_hex(3)
-        amp = self.generate_noise(token=token)
+        amp = self.generate_noise()
         amp = (amp - amp.min())
         amp = amp * 255 / (amp.max())
         fname = join(PATH_TO_DYNAMIC_NOISE_FOLDER, f'{token}.png')
