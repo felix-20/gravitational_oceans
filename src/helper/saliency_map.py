4a6df9cf08a7b834d60d97acef7980a9bc5b86e2
felix-20 <felix-20@noreply.github.com>
2023-03-18 08:45:25 +0100

    saliency map for visualizing model

---
+++ src/helper/saliency_map.py
@@ -0,0 +1,72 @@
+# https://github.com/sunnynevarekar/pytorch-saliency-maps/blob/master/Saliency_maps_in_pytorch.ipynb
+
+import timm
+import torch
+import matplotlib.pyplot as plt
+from copy import deepcopy
+from os.path import join
+
+from src.helper.utils import PATH_TO_MODEL_FOLDER, PATH_TO_CACHE_FOLDER, print_green, print_yellow
+from src.helper.dynamic_noise_visualisation import get_one_sample
+from src.data_management.datasets.realistic_dataset import GORealisticNoiseDataset
+
+
+def saliency(img, model):
+    # we don't need gradients w.r.t. weights for a trained model
+    for param in model.parameters():
+        param.requires_grad = False
+    
+    # set model in eval mode
+    model.eval()
+
+    # keep the original for displaying it later
+    org = deepcopy(img)[0]
+
+    # bring img to right shape
+    model_input = img
+    model_input.unsqueeze_(0)
+
+    # we want to calculate gradient of higest score w.r.t. input
+    # so set requires_grad to True for input 
+    model_input.requires_grad = True
+
+    # forward pass to calculate predictions
+    preds = model(model_input)
+    score, indices = torch.max(preds, 1)
+
+    # backward pass to get gradients of score predicted class w.r.t. input image
+    score.backward()
+
+    # get max along channel axis
+    slc, _ = torch.max(torch.abs(model_input.grad[0]), dim=0)
+
+    # normalize to [0..1]
+    slc = (slc - slc.min())/(slc.max()-slc.min())
+
+    #apply inverse transform on image
+    #plot image and its saleincy map
+    # plt.figure(figsize=(20, 10))
+    plt.subplot(1, 2, 1)
+    plt.imshow(org, cmap='gray')
+    plt.xticks([])
+    plt.yticks([])
+    plt.subplot(1, 2, 2)
+    plt.imshow(slc.numpy(), cmap=plt.cm.hot)
+    plt.xticks([])
+    plt.yticks([])
+    plt.savefig(join(PATH_TO_CACHE_FOLDER, 'sali.png'))
+
+
+if __name__ == '__main__':
+    model_path = join(PATH_TO_MODEL_FOLDER, 'plain_model.pth')
+    model_type = 'inception_v4'
+        
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+    model = timm.create_model(model_type, pretrained=False, num_classes=1, in_chans=2).to(device)
+    model.load_state_dict(torch.load(model_path))
+
+    image = torch.tensor(get_one_sample(should_normalize=False))
+    model = model.to(device)
+
+    saliency(image, model)
--- src/helper/signal_strength_over_accuracy_graph.py
+++ src/helper/signal_strength_over_accuracy_graph.py
@@ -6,29 +6,44 @@ from os import path
 from src.helper.utils import PATH_TO_CACHE_FOLDER
 
 if __name__ == '__main__':
-    input_path = path.join(PATH_TO_CACHE_FOLDER, 'signal_strength_results', 'signal_strength_over_accuracy_small.csv')
-    output_path = path.join(PATH_TO_CACHE_FOLDER, 'signal_strength_results', 'signal_strength_over_accuracy_small.png')
+    input_path = path.join(PATH_TO_CACHE_FOLDER, 'signal_strength_results', 'signal_strengths_paper.csv')
+    output_path = path.join(PATH_TO_CACHE_FOLDER, 'signal_strength_results', 'signal_strengths_paper.png')
 
     lines = sum(1 for _ in open(input_path))
 
     cmap = plt.get_cmap('viridis')
     colors = [cmap(i) for i in np.linspace(0, 1, lines)]
-
+    
+    fig = plt.figure()
+    ax = plt.subplot(111)
+    
     handles = []
     line_index = 0
+    all_data = []
     with open(input_path, 'r') as file:
         for line in file:
             data = line.split(',')
             data = [float(x) for x in data]
+            all_data += [data]
+    
+    all_data = sorted(all_data, key=lambda x: x[0])
 
-            signal_strength = data[0]
-            max_accuracy = data[1]
-            accuracies = data[2:]
-            epochs = len(accuracies)
+    for line in all_data:
+        signal_strength = line[0]
+        max_accuracy = line[1]
+        accuracies = line[2:]
+        epochs = len(accuracies)
+
+        handle = ax.plot(range(epochs), accuracies, label=f'{signal_strength:.5f}', color=colors[line_index])
+        handles += handle
+        line_index += 1
     
-            handle = plt.plot(range(epochs), accuracies, label=f'{signal_strength:.2f}', color=colors[line_index])
-            handles += handle
-            line_index += 1
-    
-    plt.legend(handles=handles[::-1])
+    # Shrink current axis by 20%
+    box = ax.get_position()
+    ax.set_position([box.x0, box.y0, box.width * 0.8, box.height])
+    ax.set_title('Accuracy as a function of signal strengths')
+    ax.set_xlabel('Epochs')
+    ax.set_ylabel('Accuracy')
+
+    plt.legend(handles=handles[::-1], loc='center left', bbox_to_anchor=(1, 0.5))
     plt.savefig(output_path)
