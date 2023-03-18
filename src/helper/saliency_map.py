b45300000bd76c474bb98f8c49fe745096e88557
Stroyxx <rk81ogel@gmail.com>
2023-03-18 09:47:02 +0100

    saliency on cuda + combination

--- src/helper/saliency_map.py
+++ src/helper/saliency_map.py
@@ -6,6 +6,7 @@ from os.path import join
 import matplotlib.pyplot as plt
 import timm
 import torch
+import numpy as np
 
 from src.data_management.datasets.realistic_dataset import GORealisticNoiseDataset
 from src.helper.dynamic_noise_visualisation import get_one_sample
@@ -44,17 +45,26 @@ def saliency(img, model):
     # normalize to [0..1]
     slc = (slc - slc.min())/(slc.max()-slc.min())
 
-    #apply inverse transform on image
-    #plot image and its saleincy map
-    # plt.figure(figsize=(20, 10))
-    plt.subplot(1, 2, 1)
-    plt.imshow(org, cmap='gray')
+    # apply inverse transform on image
+    # plot image and its saliency map
+    plt.figure(figsize=(40, 20))
+    plt.subplot(1, 3, 1)
+    plt.imshow(org.cpu(), cmap='gray', interpolation='none')
     plt.xticks([])
     plt.yticks([])
-    plt.subplot(1, 2, 2)
-    plt.imshow(slc.numpy(), cmap=plt.cm.hot)
+
+    plt.subplot(1, 3, 2)
+    plt.imshow(slc.cpu(), cmap=plt.cm.hot, interpolation='none')
     plt.xticks([])
     plt.yticks([])
+
+    image_tensor = torch.cat([org.cpu().unsqueeze(0), slc.cpu().unsqueeze(0), slc.cpu().unsqueeze(0)], dim=0)
+    image_array = np.transpose(np.clip(image_tensor.numpy(), 0, 1), (1, 2, 0))
+    plt.subplot(1, 3, 3)
+    plt.imshow(image_array, interpolation='none')
+    plt.xticks([])
+    plt.yticks([])
+
     plt.savefig(join(PATH_TO_CACHE_FOLDER, 'sali.png'))
 
 
@@ -67,7 +77,7 @@ if __name__ == '__main__':
     model = timm.create_model(model_type, pretrained=False, num_classes=1, in_chans=2).to(device)
     model.load_state_dict(torch.load(model_path))
 
-    image = torch.tensor(get_one_sample(should_normalize=False))
+    image = torch.tensor(get_one_sample(should_normalize=False)).to(device)
     model = model.to(device)
 
     saliency(image, model)
